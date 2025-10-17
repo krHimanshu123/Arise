3a5a9d8ae9e5fe83da669c47cd871d1d21f42495
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-17 23:17:31 +0530

    Fix formatting in ClerkHeader component

--- components/ClerkHeader.jsx
+++ components/ClerkHeader.jsx
@@ -50,6 +50,7 @@ export default function ClerkHeader() {
             <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
               Sign Up
             </button>
+            
           </SignUpButton>
         </SignedOut>
         <SignedIn>
